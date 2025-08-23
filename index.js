b676794506612b46f834eca033cba45113829a5b
kharleedbs01 <biliaminkhalid2002@gmail.com>
2025-08-23 20:26:17 +0100

    Added .env and env.example for assignment

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+
+// get port from .env or default to 3000
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
 app.get('/login', (req, res) => {
-   res.send('login successfull!')
+  res.send('login successful!')
 })
 
 app.get('/logout', (req, res) => {
-   res.send('log out successfull!')
+  res.send('log out successful!')
 })
 
-
 app.get('/forgetpassword', (req, res) => {
-   res.send('change password!')
+  res.send('change password!')
 })
 
 app.get('/application', (req, res) => {
-   res.send('Application Completed Successfully!')
+  res.send('Application Completed Successfully!')
 })
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })
-
