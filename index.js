b5c6a95401e3ecaf1f565c218ff3e42905af3028
Nabilaen-7 <nabiladuro@gmail.com>
2025-08-23 22:06:28 +0100

    Configured server to use .env for PORT

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const port = 3000
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -17,6 +20,5 @@ app.get('/register', (req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Server running on http://localhost:${port}`);
+});
